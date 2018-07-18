b029678548a58b0cd4425a1303a8f967a7912622
qiumingbao <qiumingbao@gmail.com>
2018-07-19 00:31:50 +0800

    Tomcat上传大文件 connection reset问题.

--- src/main/java/com/ibm/springmvc/fileupload/FileuploadApplication.java
+++ src/main/java/com/ibm/springmvc/fileupload/FileuploadApplication.java
@@ -1,7 +1,11 @@
 package com.ibm.springmvc.fileupload;
 
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FileuploadApplication {
@@ -9,4 +13,23 @@ public class FileuploadApplication {
     public static void main(String[] args) {
         SpringApplication.run(FileuploadApplication.class, args);
     }
+    
+    //Tomcat large file upload connection reset
+    //http://www.mkyong.com/spring/spring-file-upload-and-connection-reset-issue/
+    @Bean
+    public TomcatEmbeddedServletContainerFactory tomcatEmbedded() {
+        
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        
+        tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+            if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+                //-1 means unlimited
+                ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+            }
+        });
+        
+        return tomcat;
+        
+    }
+    
 }
