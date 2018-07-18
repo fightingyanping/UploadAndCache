02e7fcb4920e63e83f9c8285e26714eb1f5fb06d
qiumingbao <qiumingbao@gmail.com>
2018-07-19 00:31:35 +0800

    修改 rest index路径

--- src/main/java/com/ibm/springmvc/fileupload/controller/RestIndexController.java
+++ src/main/java/com/ibm/springmvc/fileupload/controller/RestIndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class RestIndexController {
     
-    @GetMapping("/rest/index")
+    @GetMapping("/rest")
     public String index() {
         return "/rest/index";
     }
